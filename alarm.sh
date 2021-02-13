d2a3a9a327edc4b22a5a40b7e7cfcb028404d153
Sumiza <64884509+Sumiza@users.noreply.github.com>
2021-02-12 21:23:55 -0500

    Update alarm.sh

--- alarm.sh
+++ alarm.sh
@@ -5,7 +5,7 @@
 source alarm.conf
 ArmingTime=$Arming_Time
 DisarmTime=$Disarm_Time
-sensorpins=($Sensor_Pins)
+SensorPins=($Sensor_Pins)
 TimeBetweenMessage=$Time_Between_Message
 PhoneNrsArm=($Phone_Numbers_Arm)
 PhoneNrsDis=($Phone_Numbers_Disarm)
@@ -13,6 +13,9 @@ PhoneNrsAlarm=($Phone_Numbers_Alarm)
 TwilioSID=$Twilio_SID
 TwilioAT=$Twilio_AH
 TwilioDiD=$Twilio_DID
+Red_led=$Led_Red
+Green_led=$Led_Green
+
 
 if [ "$ArmingTime" = "" ] || [ "$DisarmTime" = "" ] || [ "$ArmingTime" = "" ] || [ "$DisarmTime" = "" ]; then
          echo "CANT LOAD CONFIG FILE EXITING"
@@ -54,6 +57,11 @@ function alarm_trigger {
           while ls armed* > /dev/null 2>&1; do
                   echo "$sendcount"
                   #ring siren
+                if [ "$red" = "1" ]; then
+                        Red_off
+                else
+                        Red_on
+                fi
                   if [ "$sendcount" -eq "$TimeBetweenMessage" ] || [ "$sendcount" -eq 0 ]; then
                         echo "sending message and calling"
                         for i in "${PhoneNrsAlarm[@]}"; do
@@ -73,6 +81,11 @@ function system_armed {
           while [ "$arm" -ge 0 ]; do
                   if ls armed* > /dev/null 2>&1; then
                        echo "$arm"
+                       if [ "$red" = "1" ]; then
+                                Red_off
+                       else
+                                Red_on
+                       fi
                        ((arm=arm-1))
                        sleep 1
                            if [ "$arm" -eq -1 ]; then
@@ -87,7 +100,7 @@ function system_armed {
 
           if [ "$arm" -eq -1 ]; then
                   echo "ARMED"
-                  for i in "${sensorpins[@]}"; do
+                  for i in "${SensorPins[@]}"; do
                           trigger=$(cat /sys/class/gpio/gpio"$i"/value)
                           if [ "$trigger" = "1" ]; then
                                  alarm_countdown "$i"
@@ -128,9 +141,24 @@ function alarm_countdown {
           done
 }
 
+function Red_on {
+        echo "0" > /sys/class/gpio/gpio"$Red_led"/value
+        red=1
+}
+function Red_off {
+        echo "1" > /sys/class/gpio/gpio"$Red_led"/value
+        red=0
+}
+function Green_on {
+        echo "0" > /sys/class/gpio/gpio"$Green_led"/value
+}
+function Green_on {
+        echo "1" > /sys/class/gpio/gpio"$Green_led"/value
+}
+
 arm=$ArmingTime
 dis=$DisarmTime
-for i in "${sensorpins[@]}"; do
+for i in "${SensorPins[@]}"; do
           echo "Activating Pin $i"
           raspi-gpio set "$i" ip pu
           echo "$i" > /sys/class/gpio/export
@@ -138,12 +166,29 @@ for i in "${sensorpins[@]}"; do
           echo "in" > /sys/class/gpio/gpio"$i"/direction
 done
 
+echo "Activating Pin Red LED"
+echo "$Red_led" > /sys/class/gpio/export
+sleep 1.0
+echo "out" > /sys/class/gpio/gpio"$Red_led"/direction
+echo "1" > /sys/class/gpio/gpio"$Red_led"/value
+        
+echo "Activating Pin Green LED"
+echo "$Green_led" > /sys/class/gpio/export
+sleep 1.0
+echo "out" > /sys/class/gpio/gpio"$Green_led"/direction
+echo "1" > /sys/class/gpio/gpio"$Green_led"/value
+
+
 while :
 do
            if ls armed* > /dev/null 2>&1; then
+               Green off
+               Red_on
                system_armed
            else
-               system_disarmed
+                Red_off
+                Green_on
+                system_disarmed
            fi
        sleep 0.5
 done
