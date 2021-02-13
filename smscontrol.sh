05d14307dfebe8a2d6a1f12947e447652b52198a
Sumiza <64884509+Sumiza@users.noreply.github.com>
2021-02-12 19:34:16 -0500

    Update smscontrol.sh

--- smscontrol.sh
+++ smscontrol.sh
@@ -4,10 +4,12 @@ source alarm.conf
 TwilioSID=$Twilio_SID
 TwilioAT=$Twilio_AH
 TwilioDiD=$Twilio_DID
+controlnumbers=($Phone_Numbers_Control)
+controlusers=($Phone_Numbers_Users)
 
 function get_messages {
 	curl -X GET https://api.twilio.com/2010-04-01/Accounts/"$TwilioSID"/Messages.json?PageSize=1 \
-                        -u "$TwilioSID":"$TwilioAT"
+        -u "$TwilioSID":"$TwilioAT"
 }
 
 function send_message {
@@ -40,35 +42,41 @@ do
         echo "------------"
         
 		if [ "$contact" != "" ] && [ "$message" != "" ] && [ "$id" != "" ] &&  [ "$direction" = "inbound" ]; then
-			if ls armed* > /dev/null 2>&1; then
-			        if [ "$message" = "status" ]; then
-			                send_message "$contact" "Status_Armed"
-			                echo "status armed"
-				
-				elif [ "$message" = "arm" ]; then
-				        send_message "$contact" "Already_armed"
-				        
-				elif [ "$message" = "disarm" ]; then
-				        rm disarmed*
-				        touch disarmed_"$contact"
-				        rm armed*
-				fi
-			else
-			        if [ "$message" = "status" ]; then
-			                send_message "$contact" "Status_Disarmed"
-			                echo "status disarmed"
-				
-				elif [ "$message" = "arm" ]; then
-			               touch armed_"$contact"
-			               
-				elif [ "$message" = "disarm" ]; then
-				        send_message "$contact" "Already_Disarmed"
-				fi
-			fi
+			c=0
+			for i in "${controlnumbers[@]}"; do
+			        if [ "$i" = "$contact" ]; then
+                			if ls armed* > /dev/null 2>&1; then
+                			        if [ "$message" = "status" ]; then
+                			                send_message "$contact" "Status_Armed"
+                			                echo "status armed"
+                				
+                				elif [ "$message" = "arm" ]; then
+                				        send_message "$contact" "Already_armed"
+                				        
+                				elif [ "$message" = "disarm" ]; then
+                				        rm disarmed*
+                				        touch disarmed_"${controlusers[c]}"
+                				        rm armed*
+                				fi
+                			else
+                			        if [ "$message" = "status" ]; then
+                			                send_message "$contact" "Status_Disarmed"
+                			                echo "status disarmed"
+                				
+                				elif [ "$message" = "arm" ]; then
+                			                touch armed_"${controlusers[c]}"
+                			               
+                				elif [ "$message" = "disarm" ]; then
+                				        send_message "$contact" "Already_Disarmed"
+                				fi
+                			fi
+                		fi
+                		((c=c+1))
+        		done
 		fi
-        		if [ "$id" != "" ]; then
-                                curl -X DELETE https://api.twilio.com"$id" \
-                                        -u "$TwilioSID":"$TwilioAT"
-                        fi
+        	if [ "$id" != "" ]; then
+                        curl -X DELETE https://api.twilio.com"$id" \
+                        -u "$TwilioSID":"$TwilioAT"
+                fi
         sleep 15
 done
