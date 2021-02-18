6a34c039aa48310bf476b1efcc3afe98644c0adb
Sumiza <64884509+Sumiza@users.noreply.github.com>
2021-02-17 23:25:41 -0500

    Update keypad.sh

--- keypad.sh
+++ keypad.sh
@@ -52,11 +52,11 @@ function passcheck {
                         if [ "$pass" = "$passhold" ]; then
                                 if ls armed* > /dev/null 2>&1; then
                                         rm disarmed*
-                                        touch disarmed_"${passusers[$passname]}"
+                                        touch disarmed by SMS by "${passusers[$passname]}" at "$(date)"
                                         rm armed*
                                         echo "keypad disarmed by ${passusers[$passname]}"
                                 else
-                                        touch armed_"${passusers[$passname]}"
+                                        touch armed by SMS by "${passusers[$passname]}" at "$(date)"
                                         echo "keypad armed by ${passusers[$passname]}"
                                 fi
                                 passhold=""
