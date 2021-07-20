55b900c53a7e19ad3945b087b572ec988a6e2d96
Sumiza <64884509+Sumiza@users.noreply.github.com>
2021-07-19 20:46:23 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,8 @@ rm smscontrol.sh
 wget https://raw.githubusercontent.com/Sumiza/raspalarm/main/keypad.sh
 wget https://raw.githubusercontent.com/Sumiza/raspalarm/main/alarm.sh
 wget https://raw.githubusercontent.com/Sumiza/raspalarm/main/smscontrol.sh
+wget https://raw.githubusercontent.com/Sumiza/RaspAlarm/main/startup.sh
 chmod +x alarm.sh
 chmod +x keypad.sh
 chmod +x smscontrol.sh
+chmod +x startup.sh
