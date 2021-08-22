411416c6c7892aa9a0be720d740ccf26294d72f7
Sumiza <64884509+Sumiza@users.noreply.github.com>
2021-08-21 20:16:12 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,7 +7,9 @@ wget https://raw.githubusercontent.com/Sumiza/raspalarm/main/keypad.sh
 wget https://raw.githubusercontent.com/Sumiza/raspalarm/main/alarm.sh
 wget https://raw.githubusercontent.com/Sumiza/raspalarm/main/smscontrol.sh
 wget https://raw.githubusercontent.com/Sumiza/RaspAlarm/main/startup.sh
+wget https://raw.githubusercontent.com/Sumiza/RaspAlarm/main/doording.sh
 chmod +x alarm.sh
 chmod +x keypad.sh
 chmod +x smscontrol.sh
 chmod +x startup.sh
+chmod +x doording.sh
